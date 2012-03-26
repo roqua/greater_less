66af3fb4bc872db667bc15b2a1c24fc55b614fd5
Samuel Esposito <s.esposito@roqua.nl>
2012-03-26 17:24:04 +0200

    scope alias

--- lib/greater_less/string_extension.rb
+++ lib/greater_less/string_extension.rb
@@ -1,10 +1,10 @@
 class String
-  alias :old_to_f :to_f
+  alias :to_f_without_greater_less :to_f
 
   def to_f
     if self =~ GreaterLess::GREATER_LESS
       return GreaterLess.new(self)
     end
-    self.old_to_f
+    self.to_f_without_greater_less
   end
 end
